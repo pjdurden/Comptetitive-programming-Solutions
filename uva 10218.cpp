d0d566b9cedc5e3af367f833da2bd8006cea3d93
pjdurden <48603194+pjdurden@users.noreply.github.com>
2021-01-28 07:01:17 +0530

    pj

--- uva 10218.cpp
+++ uva 10218.cpp
@@ -21,7 +21,7 @@ const int mod=1e9+7;
 #define umap unordered_map
 #define uset unordered_set
 #define vvi vector<vector<int>>
-//double powmod(double x,double y){double t; for(t=1;y;y>>=1,x=x*x%mod) if(y&1) t=t*x%mod; return t;}
+ull powmod(ull x,ull y){ull t; for(t=1;y;y>>=1,x=x*x%mod) if(y&1) t=t*x%mod; return t;}
 ull gcd(ull x,ull y){return y?gcd(y,x%y):x;}
 void smask(int pos,int &i){ i=i^(1<<pos); }
 void clmask(int pos,int &i){ i=i&~(1<<pos); }
@@ -39,24 +39,28 @@ double cordist(pair<double,double> a,pair<double,double> b)
 //int dy[]={0,1,0,-1};
 //char board[26][26];
 ull n,m,q,r;
+double dp[100][1000];
 
+double counts(int ind,int count)
+{
+    if(ind==q)
+    {
+        return count%2==0;
+    }
+    if(dp[ind][count]>-0.5 )return dp[ind][count];
+    double res= ((1.0*n)/(n+m))*counts(ind+1,count+1) + ((1.0*m)/(n+m))*counts(ind+1,count);
+    return dp[ind][count]=res;
+}
 
 void solve()
 {
-    int counter=1;
-    while(cin>>m>>n and !(n==0 and m==0) )
-    {
-        printf("Case %d: ",counter++);
-        if(m<=1)
-        {
-            printf(":-%c\n",'\\');
-        }
-        else if(m<=n) 
-        {
-             printf(":-|\n");
-        }
-        else printf(":-(\n");
 
+    while( cin>>n>>m>>q and !(n==0 and m==0) )
+    {
+        memset(dp,-1,sizeof dp);
+        printf("%0.7lf\n",counts(0,0));
+        
+        
     }
 
 }
--- uva 12114.cpp
+++ uva 12114.cpp
@@ -65,12 +65,7 @@ void solve()
 
 int main()
 {
-    #ifndef ONLINE_JUDGE
-    // for getting input from input.txt
-    freopen("input.txt", "r", stdin);
-    // for writing output to output.txt
-    freopen("output.txt", "w", stdout);
-    #endif
+   
     int k=1;
     //cin>>k;
     //int counter=1;
